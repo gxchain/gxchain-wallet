ff4d80d2eabf4a4e86d53f19430d6e5ac96cc25b
lanhaoxiang <lanhaoxiang@qq.com>
2017-11-14 20:28:14 +0800

    test env is ready

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,12 @@
 module.exports = {
   NODE_ENV: '"production"',
-  __HOST__:'"https://mwallet.gxb.io/#"',
-  __SERVICE__:'"http://192.168.1.124:9218"',
-  witnesses:JSON.stringify(["wss://node1.gxb.io","wss://node5.gxb.io","wss://node8.gxb.io","wss://node11.gxb.io"]),
-  faucet_addr:'"https://opengateway.gxb.io"'
+  // __HOST__:'"https://mwallet.gxb.io/#"',
+  // __SERVICE__:'"http://192.168.1.124:9218"',
+  // witnesses:JSON.stringify(["wss://node1.gxb.io","wss://node5.gxb.io","wss://node8.gxb.io","wss://node11.gxb.io"]),
+  // faucet_addr:'"https://opengateway.gxb.io"'
+  NODE_ENV: '"development"',
+  __HOST__:'"http://47.96.164.78/#"',
+  __SERVICE__:'"http://47.96.164.78:9218"',
+  witnesses:JSON.stringify(['ws://47.96.164.78:28090']),
+  faucet_addr:'"http://47.96.164.78:8888"'
 }
