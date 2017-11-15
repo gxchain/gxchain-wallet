2278acee26e45b602665d6405da11fb79122d73f
lanhaoxiang <lanhaoxiang@qq.com>
2017-11-16 01:44:48 +0800

    build script changed

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,21 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  __HOST__:'"https://mwallet.gxb.io/#"',
-  __SERVICE__:'"https://walletgateway.gxb.io"',
-  witnesses:JSON.stringify(["wss://node1.gxb.io","wss://node5.gxb.io","wss://node8.gxb.io","wss://node11.gxb.io"]),
-  faucet_addr:'"https://opengateway.gxb.io"'
-  // __HOST__:'"http://47.96.164.78/#"',
-  // __SERVICE__:'"http://47.96.164.78:9218"',
-  // witnesses:JSON.stringify(['ws://47.96.164.78:28090']),
-  // faucet_addr:'"http://47.96.164.78:8888"'
-}
+module.exports = (function () {
+  console.log('isProd',!!process.env.prod);
+  if(!!process.env.prod){
+    return {
+      NODE_ENV: '"production"',
+      __HOST__:'"https://mwallet.gxb.io/#"',
+      __SERVICE__:'"https://walletgateway.gxb.io"',
+      witnesses:JSON.stringify(["wss://node1.gxb.io","wss://node5.gxb.io","wss://node8.gxb.io","wss://node11.gxb.io"]),
+      faucet_addr:'"https://opengateway.gxb.io"'
+    }
+  }
+  else{
+    return {
+      NODE_ENV: '"production"',
+      __HOST__:'"http://47.96.164.78/#"',
+      __SERVICE__:'"http://47.96.164.78:9218"',
+      witnesses:JSON.stringify(['ws://47.96.164.78:28090']),
+      faucet_addr:'"http://47.96.164.78:8888"'
+    }
+  }
+})()
