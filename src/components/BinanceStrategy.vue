4b0f0b4081c5fc9279cd64866784f51498d489fa
yrm <yrm@outlook.com>
2017-12-04 22:11:45 +0800

    add charts and strategy pages

---
+++ src/components/BinanceStrategy.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="popup">
+    <header class="bar bar-nav">
+      <h2 class="title">Binance充值交易平台教程</h2>
+      <a href="javascript:;" class="pull-right icon icon-close close-popup">&times;</a>
+    </header>
+    <div class="content">
+      <div class="detail">
+        <p class="step">第一步：</p>
+        <p>进入网站：https://www.binance.com/</p>
+        <img src="../../static/strategy_img/binance_step1_img1.png" width="100%"/>
+        <p>点击【注册】账户，填写信息，邮箱激活账户，完成注册</p>
+        <img src="../../static/strategy_img/binance_step1_img2.png" width="100%"/>
+        <p class="step">第二步：</p>
+        <p>注册后进入【认证】页面</p>
+        <img src="../../static/strategy_img/binance_step2_img1.png" width="100%"/>
+        <img src="../../static/strategy_img/binance_step2_img2.png" width="100%"/>
+        <p>如果通过2btc交易需要进行实名认证</p>
+        <p class="step">第三步：</p>
+        <img src="../../static/strategy_img/binance_step3_img1.png" width="100"/>
+        <p>点击【资产管理】进入【充值提现】页面</p>
+        <img src="../../static/strategy_img/binance_step3_img2.png" width="100%"/>
+        <p>点击「GXS」充值，会出现一栏警示，点击【我已知晓，确认使用】</p>
+        <img src="../../static/strategy_img/binance_step3_img3.png" width="100%"/>
+        <p>账户：binance-1</p>
+        <p>MEMO:(每个账户的MEMO不一样，请务必填写MEMO)</p>
+        <p class="step">具体充币有以下两种操作：</p>
+        <p>
+          (1):通过其他交易平台提现，向binance-1交易平台【充值】方式
+          以云币交易平台为例，点击【绑定区块链资产提现地址】
+        </p>
+        <img src="../../static/strategy_img/binance_step3_img4.png" width="100%"/>
+        <p>进入界面后：</p>
+        <p>第一栏选择【GXS】</p>
+        <p>第二栏备注，建议填写【binance平台交易账户】</p>
+        <p>第三栏输入平台交易的账户：binance-1</p>
+        <img src="../../static/strategy_img/binance_step3_img5.png" width="100%"/>
+        <p>回到【区块链资产提现】，点击binance-1这栏【提现】</p>
+        <p>MEMO : (每个账户的MEMO不一样，请务必填写MEMO)</p>
+        <p>钱包地址和账户memo最好复制以免出错</p>
+        <img src="../../static/strategy_img/binance_step3_img6.png" width="100%"/>
+        <p>提现申请成功，等待云币审核通过，近期云币提现提币申请较多，平台审核可能需要较长时间。（注册的邮箱会收到提现申请的邮件）</p>
+        <p>（2）:通过钱包向binance-1交易平台【充币】方式以下界面为在线钱包【在线钱包和钱包客户端的操作一致】</p>
+        <img src="../../static/strategy_img/binance_step3_img7.png" width="100%"/>
+        <p>点击【转账】</p>
+        <img src="../../static/strategy_img/binance_step3_img8.png" width="100%"/>
+        <p>来自：是自己的账户信息</p>
+        <p>发往：binance-1（交易平台的账户），建议复制黏贴以免操作错误</p>
+        <p>金额：往平台充值的数量，可小额充值测试</p>
+        <p>备注信息：交易平台提供的MEMO信息，MEMO信息建议从平台复制，以免出错，交易平台转账一定要填写MEMO！！！</p>
+        <img src="../../static/strategy_img/binance_step3_img9.png" width="100%"/>
+        <p>输入解锁密码，核对转账信息，确认无误后，点击【确认发送】</p>
+        <p>提醒：此次教程用于binance-1充值交易平提现</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      show() {
+        $.popup(this.$el);
+      }
+    },
+  }
+</script>
+<style lang="scss" scoped>
+  .detail{
+    padding-bottom: .6rem;
+    padding-left: .67rem;
+    padding-right: .67rem;
+    font-size: .6rem;
+    .step{
+      font-weight: bold;
+    }
+  }
+</style>
--- src/components/RealtimeQuotations.vue
+++ src/components/RealtimeQuotations.vue
@@ -9,13 +9,11 @@
         <div class="content">
           <div class="exchange">
             <div class="exchange-title">
-              <!-- <div class="asset-name">{{$t('index.asset_name')}}<small class="color-light-gray" v-if="$t('index.asset_full_name')!=''">({{$t('index.asset_full_name')}})</small></div> -->
               <div class="asset-name">{{exchange_symbol}}</div>
               <div class="exchange-name">{{exchange_name}}</div>
             </div>
             <div class="exchange-price">
               <div class="price-primary">
-                <!-- {{_i18n.locale == 'zh-CN' ? `￥${exchange_price_rmb}` : `$${exchange_price_dollar}`}} -->
                 {{exchange_price}}
               </div>
               <div class="price-secondary">
@@ -57,33 +55,50 @@
           </div>
           <div class="chart">
             <div class="buttons-row">
-              <a :class="{active:tabIndex==0}" @click="tabIndex=0"
+              <a :class="{active:tabIndex==0}" @click="loadTimeData"
                 class="tab-link button">{{$t('realtime_quotations.time_sharing')}}</a>
-              <a :class="{active:tabIndex==1}" @click="tabIndex=1"
+              <a :class="{active:tabIndex==1}" @click="loadKlineData"
                 class="tab-link button">{{$t('realtime_quotations.k_line')}}</a>
             </div>
             <div class="tabs">
               <div class="tab" :class="{active:tabIndex==0}">
-                <canvas id="time-sharing"></canvas>
+                <div id="time-sharing"></div>
               </div>
               <div class="tab" :class="{active:tabIndex==1}">
-                <canvas id="k-line"></canvas>
+                <div id="k-line"></div>
               </div>
             </div>
           </div>
           <div class="strategy">
-            <a v-show="showStrategyLink" @click="showPDFModal">{{$t('realtime_quotations.strategy')+'>'}}></a>
+            <a v-if="_i18n.locale == 'zh-CN'" @click="showStrategyModal">{{$t('realtime_quotations.strategy')+'>'}}></a>
           </div>
         </div>
       </div>
     </div>
-    <exchange-strategy  ref="strategy" :fileUrl="this.strategy_pdf_url"></exchange-strategy>
+  
+    <div v-if="exchange_name === 'Binance'">
+      <binance-strategy  ref="strategy"></binance-strategy>
+    </div>
+    <div v-else-if="exchange_name === 'Bit-Z'">
+      <bitz-strategy  ref="strategy"></bitz-strategy>
+    </div>
+    <div v-else-if="exchange_name === 'Allcoin'">
+      <allcoin-strategy  ref="strategy"></allcoin-strategy>
+    </div>
+    <div v-else-if="exchange_name === 'BigOne'">
+      <bigone-strategy  ref="strategy"></bigone-strategy>
+    </div>
+    
   </div>
 </template>
 <script>
-  import F2 from '@antv/f2'
-  import ExchangeStrategy from '@/components/ExchangeStrategy.vue'
-  import {get_realtime_quotation} from '@/services/MarketService'
+  import G2 from '@antv/g2'
+  import {View} from '@antv/data-set'
+  import BinanceStrategy from '@/components/BinanceStrategy.vue'
+  import BitzStrategy from '@/components/BitzStrategy.vue'
+  import AllcoinStrategy from '@/components/AllcoinStrategy.vue'
+  import BigoneStrategy from '@/components/BigoneStrategy.vue'
+  import {get_realtime_quotation,get_chart_data} from '@/services/MarketService'
   export default {
     data() {
       return {
@@ -97,22 +112,21 @@
         low:'',
         volume:'',
         tabIndex:0,
-        strategy_pdf_url:'',
-        showStrategyLink: true,
-        stopFetchingQuotation: false
+        stopFetchingQuotation: false,
+        timeData:[],
+        klineData:[]
       }
     },
     methods: {
       loadRealtimeQuotation(){
         get_realtime_quotation(this.exchange_name,this.exchange_symbol).then((resp) => {
-          console.log('resp:'+JSON.stringify(resp));
           this.exchange_price = resp.price;
           this.exchange_price_rmb = resp.price_rmb;
           this.exchange_price_dollar = resp.price_dollar;
-          this.exchange_quote = resp.quote,
-          this.high = resp.high,
-          this.low = resp.low,
-          this.volume = resp.volume,
+          this.exchange_quote = resp.quote;
+          this.high = resp.high;
+          this.low = resp.low;
+          this.volume = resp.volume;
           setTimeout(() => {
             if(!this.stopFetchingQuotation){
               this.loadRealtimeQuotation();
@@ -127,258 +141,204 @@
           }, 5000);
         })
       },
-      renderTimeSharing(){
-        // 设置屏幕像素比
-      F2.Global.pixelRatio = window.devicePixelRatio;
-      const data = [
-        { time: '2016-08-08 00:00:00', tem: 10 },
-        { time: '2016-08-08 00:10:00', tem: 22 },
-        { time: '2016-08-08 00:30:00', tem: 16},
-        { time: '2016-08-09 00:35:00', tem: 26},
-        { time: '2016-08-09 01:00:00', tem: 12},
-        { time: '2016-08-09 01:20:00', tem: 26},
-        { time: '2016-08-10 01:40:00', tem: 18},
-        { time: '2016-08-10 02:00:00', tem: 26},
-        { time: '2016-08-10 02:20:00', tem: 12},
-
-      ];
-      const chart = new F2.Chart({
-        id: 'time-sharing',
-        pixelRatio: window.devicePixelRatio,
-        width:window.innerWidth,
-        height:400
-      });
-      chart.source(data, {
-        time: {
-          type: 'timeCat',
-          tickCount: 3,
-          range: [ 0, 1 ]
-        },
-        tem: {
-          tickCount: 5,
-          min: 0
+      loadTimeData(){
+        this.tabIndex = 0;
+        if(this.timeData.length == 0){
+          get_chart_data(this.exchange_name,this.exchange_symbol,'1m').then((resp) => {
+          this.timeData = resp.filter((item)=>{
+            return item.volumn > 0
+          }).map((item)=>{
+            item.pair_price = parseFloat((item.price / item.volumn).toFixed(7));
+            item.time = new Date(item.timestamp).format("yyyy-MM-dd hh:mm:ss"); 
+            return item;
+          });
+          this.renderTimeSharingG2();
+          })
         }
-      });
-        // 配置刻度文字大小，供PC端显示用(移动端可以使用默认值20px)
-      chart.axis('tem', {
-        label: {
-          fontSize: 12
-        }
-      });
-        // 配置time刻度文字样式
-      const label = {
-        fill: '#979797',
-        font: '12px san-serif',
-        offset: 6
-      };
-      chart.axis('time', {
-        label(text, index, total) {
-          const cfg = label;
-            // 第一个点左对齐，最后一个点右对齐，其余居中，只有一个点时左对齐
-          if (index === 0) {
-            cfg.textAlign = 'start';
-          }
-          if (index > 0) {
-            if(index === total - 1){
-              cfg.textAlign = 'end';
-            }else{
-              cfg.textAlign = 'center';
-            }
-          }
-          return cfg;
-        }
-      });
-      chart.area().position('time*tem')
-        .shape('smooth')
-        .style({
-          opacity: 0.6
-        });
-      chart.render();
-
       },
-      renderKLine(){
-        const data = [
-          {
-            time: "2015-11-19",
-            start: 8.18,
-            max: 8.33,
-            min: 7.98,
-            end: 8.32,
-            volumn: 1810,
-            money: 14723.56
+      loadKlineData(){
+        this.tabIndex = 1;
+        if(this.klineData.length == 0){
+          get_chart_data(this.exchange_name,this.exchange_symbol,'1440m').then((resp) => {
+          this.klineData = resp.map((r) => {
+            r.time = new Date(r.timestamp).format("yyyy-MM-dd hh:mm:ss");  
+            r.max = parseFloat(r.max);
+            r.min = parseFloat(r.min);
+            r.start = parseFloat(r.start);
+            r.end = parseFloat(r.end);
+            r.volumn = parseFloat(r.volumn);
+            r.money = parseFloat(r.price);
+            return r;
+          });
+          this.renderKLineG2();
+          })
+        }
+      },
+      renderKLineG2(){
+        const dv = new View();
+        dv.source(this.klineData)
+        .transform({
+          type: 'map',
+          callback: obj => {
+            obj.trend = (obj.start <= obj.end) ? '上涨' : '下跌';
+            obj.range = [ obj.start, obj.end, obj.max, obj.min ];
+            return obj;
+          }
+        });
+        const chart = new G2.Chart({
+          container: 'k-line',
+          forceFit: true,
+          height: 420,
+          animate: true,
+        });
+        chart.source(dv, {
+          'time': {
+            type: 'timeCat',
+            nice: false,
+            tickCount: 5,
+            mask: 'MM-DD',
+            range: [ 0, 1 ]
           },
-          {
-            time: "2015-11-18",
-            start: 8.37,
-            max: 8.6,
-            min: 8.03,
-            end: 8.09,
-            volumn: 2790.37,
-            money: 23309.19
+          trend: {
+            values: [ '上涨', '下跌' ]
           },
-          {
-            time: "2015-11-17",
-            start: 8.7,
-            max: 8.78,
-            min: 8.32,
-            end: 8.37,
-            volumn: 3729.04,
-            money: 31709.71
-          },
-          {
-            time: "2015-11-16",
-            start: 8.18,
-            max: 8.69,
-            min: 8.05,
-            end: 8.62,
-            volumn: 3095.44,
-            money: 26100.69
-          },
-          {
-            time: "2015-11-13",
-            start: 8.01,
-            max: 8.75,
-            min: 7.97,
-            end: 8.41,
-            volumn: 5815.58,
-            money: 48562.37
-          },
-          {
-            time: "2015-11-12",
-            start: 7.76,
-            max: 8.18,
-            min: 7.61,
-            end: 8.15,
-            volumn: 4742.6,
-            money: 37565.36
-          },
-          {
-            time: "2015-11-11",
-            start: 7.55,
-            max: 7.81,
-            min: 7.49,
-            end: 7.8,
-            volumn: 3133.82,
-            money: 24065.42
-          },
-          {
-            time: "2015-11-10",
-            start: 7.5,
-            max: 7.68,
-            min: 7.44,
-            end: 7.57,
-            volumn: 2670.35,
-            money: 20210.58
-          },
-          {
-            time: "2015-11-09",
-            start: 7.65,
-            max: 7.66,
-            min: 7.3,
-            end: 7.58,
-            volumn: 2841.79,
-            money: 21344.36
-          },
-          {
-            time: "2015-11-06",
-            start: 7.52,
-            max: 7.71,
-            min: 7.48,
-            end: 7.64,
-            volumn: 2725.44,
-            money: 20721.51
-          },
-        ];
+          'volumn': {alias: '成交量'},
+          'start': {alias: '开盘价'},
+          'end': {alias: '收盘价'},
+          'max': {alias: '最高价'},
+          'min': {alias: '最低价'},
+          'range': {alias: '价格'}
+        });
+        chart.legend({
+          offset: 20
+        });
+        chart.tooltip({
+          showTitle: false,
+          itemTpl: '<li data-index={index}>'
+          + '<span style="background-color:{color};" class="g2-tooltip-marker"></span>'
+          + '{name}{value}</li>',
+        });
+        const kView = chart.view({
+          end: {
+            x: 1,
+            y: 0.5
+          }
+        });
+        kView.source(dv);
+        kView.schema()
+          .position('time*range')
+          .color('trend', val => {
+            if (val === '上涨') {
+              return '#f04864';
+            }
 
-        // 设置屏幕像素比
-        F2.Global.pixelRatio = window.devicePixelRatio;
-        //数据处理
-        data.sort(function(obj1, obj2) {
-          return obj1.time > obj2.time ? 1 : -1;
-        });
-        data.forEach(function(obj) {
-          obj.range = [ obj.start, obj.end, obj.max, obj.min ];
-          obj.trend = (obj.start <= obj.end) ? 0 : 1;
-        });
-        const chart = new F2.Chart({
-          id: 'k-line',
-          width:window.innerWidth,
-          height:400
-        });
-        // 配置刻度文字大小，供PC端显示用(移动端可以使用默认值20px)
-        chart.axis('range', {
-          label: {
-            fontSize: 12
+            if (val === '下跌') {
+              return '#2fc25b';
+            }
+          })
+          .shape('candle')
+          .tooltip('time*start*end*max*min', (time, start, end, max, min) => {
+            return {
+              name: time,
+              value: '<br><span style="padding-left: 16px">开盘价：' + start + '</span><br/>'
+              + '<span style="padding-left: 16px">收盘价：' + end + '</span><br/>'
+              + '<span style="padding-left: 16px">最高价：' + max + '</span><br/>'
+              + '<span style="padding-left: 16px">最低价：' + min + '</span>'
+            };
+          });
+        const barView = chart.view({
+          start: {
+            x: 0,
+            y: 0.65
           }
         });
-        // 配置time刻度文字样式
-        const label = {
-          fill: '#979797',
-          font: '12px san-serif',
-          offset: 6
-        };
-        chart.axis('time', {
-          label(text, index, total) {
-            const cfg = label;
-            // 第一个点左对齐，最后一个点右对齐，其余居中，只有一个点时左对齐
-            if (index === 0) {
-              cfg.textAlign = 'start';
-            }
-            if (index > 0) {
-              if(index === total - 1){
-                cfg.textAlign = 'end';
-              }else{
-                cfg.textAlign = 'center';
-              }
-            }
-            return cfg;
-          }
-        });
-        chart.source(data, {
-          range: {
-            tickCount: 5
-          },
-          time: {
+        barView.source(dv, {
+          volumn: {
             tickCount: 3
           }
         });
-        chart.schema().position('time*range')
-          .color('trend', function(trend) {
-            return [ '#C00000', '#19B24B' ][trend];
-          })
-          .shape('candle');
+        barView.axis('time', {
+          tickLine: null,
+          label: null
+        });
+        barView.axis('volumn', {
+          label: {
+            formatter: function(val) {
+              return parseInt(val / 10000, 10) + 'w';
+            }
+          }
+        });
+        barView.interval()
+        .position('time*volumn')
+        .color('trend',  val => {
+          if (val === '上涨') {
+            return '#f04864';
+          }
+
+          if (val === '下跌') {
+            return '#2fc25b';
+          }
+        })
+        .tooltip('time*volumn', (time, volumn) => {
+          return {
+            name: time,
+            value: '<br/><span style="padding-left: 16px">成交量：' + volumn + '</span><br/>'
+          };
+        });
+
+        chart.render();
+
+
+      },
+      renderTimeSharingG2(){
+        const chart = new G2.Chart({
+          container: 'time-sharing',
+          forceFit: true,
+          height: 420
+        });
+        chart.source(this.timeData);
+        chart.scale({
+          pair_price: {
+            // min: 0.0002,
+            tickCount: 5,
+          },
+          time: {
+            type: 'timeCat',
+            tickCount: 5,
+            mask: 'HH:mm',
+            range: [ 0, 1 ]
+          }
+        });
+        chart.axis('pair_price', {
+          label: {
+            formatter: val => {
+              return val
+            }
+          }
+        });
+        chart.tooltip({
+          crosshairs: {
+            type: 'line'
+          }
+        });
+        chart.area().position('time*pair_price');
+        chart.line().position('time*pair_price').size(1);
         chart.render();
       },
-      showPDFModal(){
-        this.getPDFUrl();
+      showStrategyModal(){
         this.$refs.strategy.show();
       },
-      isShowStrategyLink(){
-        if(this.exchange_name === 'Bit-Z' || this.exchange_name === 'AllCoin' || this.exchange_name ==='Binance'){
-          this.showStrategyLink = true;
-        }else {
-          this.showStrategyLink = false;
-        }
-      },
-      getPDFUrl(){
-        if(this.exchange_name === 'Bit-Z'){
-          this.strategy_pdf_url = 'https://gxs-wallet.oss-cn-shanghai.aliyuncs.com/bit-z.pdf';
-        }else if(this.exchange_name === 'AllCoin'){
-          this.strategy_pdf_url = 'https://gxs-wallet.oss-cn-shanghai.aliyuncs.com/allcoin.pdf';
-        }else if(this.exchange_name === 'Binance'){
-          this.strategy_pdf_url = 'https://gxs-wallet.oss-cn-shanghai.aliyuncs.com/binance.pdf';
-        }
-      }
     },
     mounted() {
       $.init();
       this.loadRealtimeQuotation();
-      this.isShowStrategyLink();
-      this.renderTimeSharing();
-      this.renderKLine();
+      this.loadTimeData();
     },
     components: {
-      ExchangeStrategy
+      BinanceStrategy,
+      BitzStrategy,
+      AllcoinStrategy,
+      BigoneStrategy
     }
   }
 
@@ -473,6 +433,16 @@
         background: #4a586f;
         color:#fff;
       }
+    }
+    .tab{
+      width:100%;
+      height:400px;
+    }
+    #time-sharing{
+      margin-top:1rem;
+    }
+    #k-line{
+      margin-top:1rem;
     }
   }
   .strategy{
