8be5836e7660101417a6f5c4828a5d700578534c
lanhaoxiang <lanhaoxiang@qq.com>
2018-05-23 20:22:46 +0800

    add node23 for west-eur users

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,7 +5,7 @@ module.exports = (function () {
       NODE_ENV: '"production"',
       __HOST__:'"https://mwallet.gxb.io/#"',
       __SERVICE__:'"https://walletgateway.gxb.io"',
-      witnesses:JSON.stringify(["wss://node1.gxb.io","wss://node5.gxb.io","wss://node8.gxb.io","wss://node11.gxb.io","wss://node15.gxb.io","wss://node16.gxb.io","wss://node17.gxb.io"]),
+      witnesses:JSON.stringify(["wss://node1.gxb.io","wss://node5.gxb.io","wss://node23.gxb.io","wss://node8.gxb.io","wss://node11.gxb.io","wss://node15.gxb.io","wss://node16.gxb.io","wss://node17.gxb.io"]),
       faucet_addr:'"https://opengateway.gxb.io"'
     }
   }
