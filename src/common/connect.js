6360120aab8301555a74471d2a5a700a584abbc4
lanhaoxiang <lanhaoxiang@qq.com>
2018-12-11 11:26:10 +0800

    chore: optimize reconnection logics

--- src/common/connect.js
+++ src/common/connect.js
@@ -1,4 +1,4 @@
-import {Apis, Manager} from 'gxbjs-ws';
+import {Manager} from 'gxbjs-ws';
 
 let witnesses = process.env.witnesses;
 let connectionManager = null;
@@ -14,28 +14,34 @@ let connect = function (callback) {
         return callback(connected);
     }
     if (!connectionManager) {
-        connectionManager = new Manager({url: witnesses[0], urls: witnesses});
+        connectionManager = new Manager({url: witnesses[0], urls: witnesses, autoFallback: true});
     }
     connectionManager.connectWithFallback(true).then(() => {
         callback && callback(connected);
+    }).catch(ex => {
+        console.error(ex);
+        setTimeout(() => {
+            console.log('>>> reconnect');
+            connect();
+        }, 1000);
     });
 };
 
 // websocket 状态处理
-Apis.setRpcConnectionStatusCallback(function (status) {
-    console.log('Witness status:', status);
-    if (!connected && status == 'open') {
-        connected = true;
-    }
-
-    if (connected && (status == 'closed' || status == 'error')) { // 出错重连
-        Apis.reset();
-        console.log('Connection failed,try another connection');
-        connected = false;
-        setTimeout(() => {
-            connect();
-        }, 1000);
-    }
-});
+// Apis.setRpcConnectionStatusCallback(function (status) {
+//     console.log('Witness status:', status);
+//     if (!connected && status == 'open') {
+//         connected = true;
+//     }
+//
+//     if (connected && (status == 'closed' || status == 'error')) { // 出错重连
+//         Apis.reset();
+//         console.log('Connection failed,try another connection');
+//         connected = false;
+//         setTimeout(() => {
+//             connect();
+//         }, 1000);
+//     }
+// });
 
 export default connect;
