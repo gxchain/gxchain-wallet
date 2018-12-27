6fbd1261486a3680c19c921c2a1c48205a1fd12f
xLogic <xloigc@qq.com>
2018-12-27 17:25:01 +0800

    chore: add config for yfb

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,14 +1,25 @@
 module.exports = (function () {
     console.log('isProd', !!process.env.prod);
     if (!!process.env.prod) {
-        return {
-            NODE_ENV: '"production"',
-            __HOST__: '"https://mwallet.gxb.io/#"',
-            __SERVICE__: '"https://walletgateway.gxb.io"',
-            witnesses: JSON.stringify(["wss://node1.gxb.io", "wss://node5.gxb.io", "wss://node23.gxb.io", "wss://node8.gxb.io", "wss://node11.gxb.io", "wss://node15.gxb.io", "wss://node16.gxb.io", "wss://node17.gxb.io"]),
-            faucet_addr: '"https://opengateway.gxb.io"',
-            chain_id: '"4f7d07969c446f8342033acb3ab2ae5044cbe0fde93db02de75bd17fa8fd84b8"'
-        };
+        if (process.env.prod == 2) {
+            return {
+                NODE_ENV: '"production"',
+                __HOST__: '"https://mwallet.gxb.io/#"',
+                __SERVICE__: '"https://walletgateway.gxb.io"',
+                witnesses: JSON.stringify(["wss://node1.gxb.io", "wss://node5.gxb.io", "wss://node23.gxb.io", "wss://node8.gxb.io", "wss://node11.gxb.io", "wss://node15.gxb.io", "wss://node16.gxb.io", "wss://node17.gxb.io"]),
+                faucet_addr: '"https://opengateway.gxb.io"',
+                chain_id: '"c2af30ef9340ff81fd61654295e98a1ff04b23189748f86727d0b26b40bb0ff4"'
+            };
+        } else {
+            return {
+                NODE_ENV: '"production"',
+                __HOST__: '"https://mwallet.gxb.io/#"',
+                __SERVICE__: '"https://walletgateway.gxb.io"',
+                witnesses: JSON.stringify(["wss://node1.gxb.io", "wss://node5.gxb.io", "wss://node23.gxb.io", "wss://node8.gxb.io", "wss://node11.gxb.io", "wss://node15.gxb.io", "wss://node16.gxb.io", "wss://node17.gxb.io"]),
+                faucet_addr: '"https://opengateway.gxb.io"',
+                chain_id: '"4f7d07969c446f8342033acb3ab2ae5044cbe0fde93db02de75bd17fa8fd84b8"'
+            };
+        }
     }
     else {
         return {
