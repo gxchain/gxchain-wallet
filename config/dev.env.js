d9dbbd3dc1828a5a323d3ddbad01ac9ab628f0c3
zhangjunjie <haifuncn@gmail.com>
2020-03-27 18:00:04 +0800

    chore: add staking sum

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,11 +2,13 @@ var merge = require('webpack-merge');
 var prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    __HOST__: '"http://localhost:8082/#"',
-    __SERVICE__: '"http://dev.gxchain.cn/gateway"',
-    witnesses: JSON.stringify(["wss://testnet.gxchain.org"]),
-    faucet_addr: '"https://testnet.faucet.gxchain.org"',
-    chain_id: '"c2af30ef9340ff81fd61654295e98a1ff04b23189748f86727d0b26b40bb0ff4"',
-    network: '"Test Network"'
+  NODE_ENV: '"development"',
+  __HOST__: '"http://localhost:8082/#"',
+  __SERVICE__: '"http://dev.gxchain.cn/gateway"',
+  witnesses: JSON.stringify(['wss://testnet.gxchain.org']),
+  faucet_addr: '"https://testnet.faucet.gxchain.org"',
+  chain_id:
+    '"c2af30ef9340ff81fd61654295e98a1ff04b23189748f86727d0b26b40bb0ff4"',
+  network: '"Test Network"',
+  staking_sum: '"https://testnet.wallet.gxchain.org"'
 });
